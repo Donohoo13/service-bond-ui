cd320fc03ef556db75614fe7bbf4dd7e3c1dcdcb
Conner Donohoo <cdonohoo@conners-mbp.lan>
2021-01-25 21:06:57 -0700

    Root structure

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="test">
+    <b-button variant="danger">Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
